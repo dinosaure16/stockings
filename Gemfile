6c77d4beca5ab7cddf1fcb97cb8c4495ac9689cc
Yu Cai <yucai1992@hotmail.com>
2021-06-28 16:27:04 +0800

    installed devise

--- Gemfile
+++ Gemfile
@@ -5,18 +5,17 @@ ruby '2.7.3'
 
 
 gem 'rails', '~> 6.1.4'
-
 gem 'sqlite3', '~> 1.4'
-
 gem 'puma', '~> 5.0'
-
 gem 'sass-rails', '>= 6'
-
 gem 'webpacker', '~> 5.0'
-
 gem 'turbolinks', '~> 5'
-
 gem 'jbuilder', '~> 2.7'
+gem 'jquery-rails'
+gem 'bootstrap', '~> 5.0', '>= 5.0.1'
+gem 'jquery-rails'
+gem 'stock_quote', '~> 3.0'
+gem 'devise', '~> 4.8'
 
 gem 'bootsnap', '>= 1.4.4', require: false
 
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,28 @@
 class HomeController < ApplicationController
   def index
+    @api = StockQuote::Stock.new(api_key: 'pk_a43df2b4dba84345833cef4ffaeb78d9' ) 
+
+    if params[:ticker] == ""
+      @nothing = "Please Enter A Symbol"
+    elsif params[:ticker]
+
+      begin
+    @stock = StockQuote::Stock.quote(params[:ticker])
+  rescue RuntimeError
+      @error = "Hey! That Stock Symbol doesn't exist. Please try again"
+
+    else
+    p "No error"
+
+    ensure
+    p "Done"
+
+
+    end
   end
+end
+
+  def about
+  end
+  
 end
